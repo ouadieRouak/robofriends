18eb15225a612f90ddd07b714b076d2c14ec5bfb
MacAir <macair@MacBook-Air-de-MacAir.local>
2020-06-05 21:29:55 +0100

    correcting app.js

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,7 @@ import React from 'react';
 import Card from './Card'
 
 const CardList=({robots})=>{
-	// if(true){
-	// 	throw new Error('Nooooo!');
-	// }
+	
 	const cardsArray = robots.map((user,i)=>{
 		return <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
 	});
